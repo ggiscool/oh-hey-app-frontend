06aaea7b29bab98612c310bfaba80e0ed4631c79
GG Gonzalez <gg.gonzalez@ymail.com>
2018-01-22 10:10:35 -0500

    login and reg rowkring with modal

--- public/app.js
+++ public/app.js
@@ -5,8 +5,34 @@ app.controller('MainController', ['$http', function($http) {
   this.test = "test";
   this.url = "http://localhost:3000";
   this.user = {};
+	this.loggedIn = false;
+  this.loginForm = false;
+	this.regForm = false;
+  this.err = '';
 
 
+//LOGIN/OUT/SIGNUP FORMS---------------
+  this.openLoginForm = () => {
+
+		this.err = '';
+
+		if (this.loginForm == false) {
+			this.loginForm = true;
+		} else {
+			this.loginForm = false;
+		}
+	}
+
+	this.openRegForm = () => {
+
+		this.err = '';
+
+		if (this.regForm == false) {
+			this.regForm = true;
+		} else {
+			this.regForm = false;
+		}
+	}
 
 //Authentication--------------
 this.login = (userPass) => {
@@ -14,13 +40,45 @@ this.login = (userPass) => {
 	$http({
 	 method: 'POST',
 	 url: this.url + '/users/login',
+   // url: this.herokuUrl + '/users/login',
 	 data: { user: { username: userPass.username, password: userPass.password }},
  }).then(response => {
+   if (response.data.status == 200) {
    console.log(response);
 		 this.user = response.data.user;
+     this.loggedIn = true;
 		 localStorage.setItem("token", JSON.stringify(response.data.token));
-		 // this.formData = {username: this.user.username}
+		 this.formData = {username: this.user.username}
+     // this.openLoginForm();
+   }
+   else {
+		this.err = 'Username and/or Password Incorrect';
+	 }
  });
+};
+
+this.createUser = (userPass) => {
+
+	$http({
+	 method: 'POST',
+	 // url: this.herokuUrl + '/users',
+	 url: this.url + '/users',
+	 data: { user: { username: userPass.username, password: userPass.password }},
+ }).then(response => {
+
+	 this.user = response.data.user;
+	 this.loggedIn = true;
+	 this.formData = {username: this.user.username}
+
+	 if (response.status == 200) {
+
+	 	// this.openSignUpForm();
+	 }
+ }).catch(reject => {
+
+
+		this.err = 'Username Already Exists';
+	});
 };
 
 this.getUsers = () => {
@@ -46,5 +104,5 @@ this.logout = () => {
   location.reload();
   this.loggedIn = false;
 }
-
+//END Authentication----------------
 }]);
