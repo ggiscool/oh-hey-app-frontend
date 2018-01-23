aa95cbef499c23a06f0b02838c82eb203f7e864a
GG Gonzalez <gg.gonzalez@ymail.com>
2018-01-22 21:51:46 -0500

    randomizer working

--- public/app.js
+++ public/app.js
@@ -9,6 +9,62 @@ app.controller('MainController', ['$http', function($http) {
   this.loginForm = false;
 	this.regForm = false;
   this.err = '';
+  this.currentquestion = false;
+  this.questionContent = '';
+  this.categories = [];
+	this.questions = [];
+
+
+  // Get categories
+  	$http({
+  		method: 'GET',
+  		// url: this.herokuUrl + '/categories'
+  		url: this.url + '/categories'
+  	}).then(response => {
+  		this.categories = response.data;
+  	}).catch(reject => {
+  	});
+
+  // Get Questions
+  	$http({
+  		method: 'GET',
+  		// url: this.herokuUrl + '/categories/1/questions'
+  		url: this.url + '/categories/1/questions'
+  	}).then(response => {
+  		this.questions = response.data;
+  	}).catch(reject => {
+  		console.log('Reject: ', reject);
+  	});
+
+  	this.displayForm = (question) => {
+  		this.err = '';
+  		if (this.display == false) {
+  			this.display = true;
+  			this.questionContent = question.content;
+  		}
+  		else {
+  			this.display = false;
+  		}
+  	};
+
+    //Randomize questions
+    this.randomQs = () => {
+      const blurbs = new Array('poop','bingo','dilly dilly','tacos','puppers','fries','shazam','right-o','steagles','brooklyn','pasta','beyonce');
+       // const i = i;
+       for (i=0; i<blurbs.length; i++){
+         const newQs = blurbs[Math.floor(Math.random() * blurbs.length)];
+         document.getElementById('blurbshere').innerHTML = newQs;
+       }
+     };
+
+  	this.category = (num) => {
+  		this.category = num;
+  		this.questionID = 0;
+  	}
+
+  	this.pickQuestion = (question) => {
+  		this.questionID = question.id;
+  	}
 
 
 //LOGIN/OUT/SIGNUP FORMS---------------
